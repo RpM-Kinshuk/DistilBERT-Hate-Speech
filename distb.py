66ebd68f00cfb85a4f124b6ef37710832726d4d8
Kinshuk Goel <103813028+RpM-Kinshuk@users.noreply.github.com>
2023-11-09 21:48:46 +0530

    Revised code and results

--- distb.py
+++ distb.py
@@ -1,4 +1,5 @@
 import argparse
+from functools import cache
 import random
 import numpy as np
 from sklearn import preprocessing
@@ -32,14 +33,17 @@ from transformers import (
     DistilBertPreTrainedModel,
     DistilBertModel,
     DistilBertTokenizer,
+    DistilBertForSequenceClassification,
 )
+
+os.environ['TRANSFORMERS_CACHE']='/rscratch/tpang/kinshuk/cache'
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument(
     "--data_path",
     type=str,
-    default="dataset/english/en_train.csv",
+    default="/rscratch/tpang/kinshuk/RpMKin/distilbert/dataset/english/data.csv",
     help="path for data file",
 )
 parser.add_argument("--seed", type=int, default=42, help="random seed")
@@ -114,8 +118,7 @@ class DistSpeech(DistilBertPreTrainedModel):
                 # loss_fct = BCEWithLogitsLoss()
                 loss = loss_fct(logits.view(-1), labels.view(-1))
             else:
-                # loss_fct = CrossEntropyLoss()
-                loss_fct = BCEWithLogitsLoss()
+                loss_fct = CrossEntropyLoss()
                 loss = loss_fct(
                     logits.view(-1, self.num_labels), labels.view(-1, self.num_labels)
                 )
@@ -138,6 +141,7 @@ def preprocess(input_text, tokenizer, max_length=128):
         add_special_tokens=True,
         max_length=max_length,
         padding="max_length",
+        truncation=True,  # Ensure sequences are truncated to the specified max_length
         return_attention_mask=True,
         return_tensors="pt",
     )
@@ -164,33 +168,21 @@ def get_train_val(args, df, batch_size=32, val_ratio=0.2, fraction=1):
     tokenizer = AutoTokenizer.from_pretrained(
         "distilbert-base-uncased", do_lower_case=True
     )
+
     token_id = []
     attention_masks = []
 
     max_len = 128
 
+    # Tokenize all the samples
     for sample in text:
         encoding_dict = preprocess(sample, tokenizer, max_len)
         token_id.append(encoding_dict["input_ids"])
         attention_masks.append(encoding_dict["attention_mask"])
 
-    # token_id = torch.cat(token_id, dim=0)
-    # attention_masks = torch.cat(attention_masks, dim=0)
-
-    # TENSORS MUST BE OF SAME SIZE ERROR
-    # token_id = torch.nn.functional.pad(torch.stack(token_id), pad=(0, max_len - token_id[0].shape[0]))
-    # attention_masks = torch.nn.functional.pad(torch.stack(attention_masks), pad=(0, max_len - attention_masks[0].shape[0]))
-
-    # TENSORS MUST BE OF SAME DIMS ERROR
-    token_id = torch.stack(
-        [torch.cat((t, torch.zeros(max_len - t.shape[0]))[:max_len]) for t in token_id]
-    )
-    attention_masks = torch.stack(
-        [
-            torch.cat((t, torch.zeros(max_len - t.shape[0]))[:max_len])
-            for t in attention_masks
-        ]
-    )
+    # Convert the lists of tensors into a single tensor
+    token_id = torch.stack([t.squeeze(0) for t in token_id])
+    attention_masks = torch.stack([t.squeeze(0) for t in attention_masks])
 
     labels = torch.tensor(labels)
 
@@ -222,7 +214,7 @@ def getModel(model_name, num_labels):
     model
     """
     config = AutoConfig.from_pretrained(model_name, num_labels=num_labels)
-    model = DistilBertModel.from_pretrained(model_name, config=config)
+    model = DistilBertForSequenceClassification.from_pretrained(model_name, config=config)
 
     for param in model.parameters():  # type: ignore
         param.requires_grad = True
@@ -233,94 +225,76 @@ def val_loss(model, data_loader, device):
     """
     val loss
     """
+    val_acc = 0
     model.eval()
     total_loss = 0
+    input_len = 0
     for batch in tqdm(data_loader):
-        batch = {k: v.to(device) for k, v in batch.items()}
+        # batch = {k: v.to(device) for k, v in batch.items()}
         with torch.no_grad():
-            outputs = model(**batch)
-            loss = outputs[0]
+            input_ids, attention_mask, labels = batch
+            input_ids = input_ids.to(device)
+            attention_mask = attention_mask.to(device)
+            labels = labels.to(device)
+            outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+            input_len += len(input_ids)
+            logits = outputs.logits
+            logits = logits.to(device)
+            loss_fn = nn.CrossEntropyLoss()  # For multi-class classification
+            loss = loss_fn(logits, labels)
             total_loss += loss.item()
-    return total_loss / len(data_loader)
-
-
-def train_loss(model, data_loader, optimizer, device):
-    """
-    train loss
-    """
-    model.train()
-    total_loss = 0
-    for batch in tqdm(data_loader):
-        batch = {k: v.to(device) for k, v in batch.items()}
-        outputs = model(**batch)
-        loss = outputs[0]
-        total_loss += loss.item()
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-    return total_loss / len(data_loader)
+        val_acc += (logits.argmax(-1) == labels).sum().item()
+    return total_loss / len(data_loader), val_acc / input_len
 
 
 def tr_loss(model, train_dataloader, val_dataloader, optimizer, device):
     """
     train loss
     """
+    tr, val = [], []
+    acc = []
     num_steps = args.epochs * len(train_dataloader)
 
     progress_bar = tqdm(
         range(num_steps),
-        disable=not args.verbose,
+        disable=False
     )
 
     for epoch in range(args.epochs):
-        for step, batch in enumerate(train_dataloader):
-            tr_loss = 0
-            val_loss = 0
-            num_all_points = 0
-            nb_tr_examples, nb_tr_steps = 0, 0
-            nb_val_examples, nb_val_steps = 0, 0
-            model.train()
-            batch = tuple(t.to(device) for t in batch)
-            b_input_ids, b_input_mask, b_labels = batch
-            b_labels = b_labels.to(device)
-            b_input_ids = b_input_ids.to(device)
-            b_input_mask = b_input_mask.to(device)
-            input_size = len(b_labels)
+
+        if epoch == 0:
+            vl, va = val_loss(model, val_dataloader, device)
+            print(f"Epoch {epoch+1}, Val Loss: {vl}, Val Acc: {va}")
+            val.append(vl)
+            acc.append(va)
+
+        model.train()
+        total_loss = 0
+    
+        for batch in train_dataloader:
+            input_ids, attention_mask, labels = batch
+            input_ids = input_ids.to(device)
+            attention_mask = attention_mask.to(device)
+            labels = labels.to(device)
             optimizer.zero_grad()
-            # Forward pass
-            train_output = model(
-                b_input_ids,
-                attention_mask=b_input_mask,
-                token_type_ids=None,
-                labels=b_labels,
-            )
-            # Backward pass
-            train_output.loss.backward()
+            outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+            logits = outputs.logits
+            loss_fn = nn.CrossEntropyLoss()  # For multi-class classification
+            loss = loss_fn(logits, labels)
+            loss.backward()
             optimizer.step()
-            # Update tracking variables
-            tr_loss += train_output.loss.item()
-            nb_tr_examples += b_input_ids.size(0)
-            num_all_points += b_input_ids.size(0)
-            nb_tr_steps += 1
+
+            total_loss += loss.item()
+
             progress_bar.update(1)
-
-
-def train(model, train_loader, val_loader, optimizer, device, epochs):
-    """
-    train
-    """
-    best_loss = np.inf
-    for epoch in range(epochs):
-        print(f"Epoch {epoch + 1}/{epochs}")
-        train_loss_ = train_loss(model, train_loader, optimizer, device)
-        print(f"Train loss {train_loss_}")
-        v_loss = val_loss(model, val_loader, device)
-        print(f"Val loss {v_loss}")
-        if v_loss < best_loss:
-            best_loss = v_loss
-            torch.save(model.state_dict(), "best_model.pth")
-            print("Saved best model")
-
+        tr_loss = total_loss / len(train_dataloader)
+        print(f"Epoch {epoch+1}, Train Loss: {tr_loss}")
+        v_loss, v_acc = val_loss(model, val_dataloader, device)
+        print(f"Epoch {epoch+1}, Val Loss: {v_loss}, Val Acc: {v_acc}")
+        tr.append(tr_loss)
+        val.append(v_loss)
+        acc.append(v_acc)
+    return tr, val, acc
 
 def main():
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -329,7 +303,17 @@ def main():
     """
     file_path = args.data_path
     # extract dataframe out of csv
-    df = pd.read_csv(file_path)
+    try:
+        df = pd.read_csv(file_path)
+        # Check if the 'text' and 'label' columns exist
+        if 'text' not in df.columns or 'label' not in df.columns:
+            raise ValueError("CSV file does not contain required 'text' and 'label' columns.")
+    except ValueError as e:
+        print(e)
+        print("The available columns in the CSV are: ", df.columns)
+        return  # Exit the program if the required columns are not found
+
+    # If the columns exist, continue with the rest of the code
     df = df[["text", "label"]]
     print("data frame is:\n{}".format(df[:5]))
     print("Number of data samples:\n{}".format(len(df)))
@@ -337,24 +321,34 @@ def main():
     train_dataloader, val_dataloader = get_train_val(
         args, df, batch_size=32, val_ratio=0.2, fraction=1
     )
+
+    df_val = pd.read_csv("/rscratch/tpang/kinshuk/RpMKin/distilbert/dataset/english/data.csv")
+    df_val = df_val[["text", "label"]]
+    print("data frame is:\n{}".format(df_val[:5]))
+    print("Number of data samples:\n{}".format(len(df_val)))
+
+    # cll, vll = get_train_val(
+    #     args, df_val, batch_size=32, val_ratio=0.2, fraction=1
+    # )
     print("Training data size: ", len(train_dataloader))
     print("Validation data size: ", len(val_dataloader))
-    return
-
-    model = getModel("distilbert-base-uncased", 1)
+    model = getModel("distilbert-base-uncased", 3)
     model.to(device)  # type: ignore
     print("Model loaded\n")
     print("Getting optimizer...")
     optimizer = getOptimizer(model, 1e-5, 0.01)
     print("Optimizer loaded\n")
     print("Training...")
-    tr_loss(model, train_dataloader, val_dataloader, optimizer, device)
+    train_loss, val_loss, val_acc = tr_loss(model, train_dataloader, val_dataloader, optimizer, device)
     print("Training complete\n")
-    print("Saving model...")
-    torch.save(model.state_dict(), "model.pth")  # type: ignore
-    print("Model saved\n")
-    print("Done")
-
+    base = {
+        "train_loss": train_loss,
+        "val_loss": val_loss,
+        "val_acc": val_acc,
+    }
+    svpath = 'rscratch/tpang/kinshuk/RpMKin/distilbert/results'
+    Path(svpath).mkdir(parents=True, exist_ok=True)
+    np.save(os.path.join(svpath, "baseline.npy"), base)
 
 if __name__ == "__main__":
     main()
