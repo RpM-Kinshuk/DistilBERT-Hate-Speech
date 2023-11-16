f94603758d9e52d88432a566afbf50f06ec99e08
Jeevansh Gagroo <87897223+gagroo-jeevansh@users.noreply.github.com>
2023-11-16 23:51:19 +0530

    shuffle fix update attack_run_base.py

--- attack_run_base.py
+++ attack_run_base.py
@@ -7,16 +7,21 @@ from textattack.attack_recipes import TextFoolerJin2019
 from textattack import AttackArgs, Attacker
 
 class CustomDataset(Dataset):
-    def __init__(self, csv_file):
+    def __init__(self, csv_file, shuffle=False):
         self.df = pd.read_csv(csv_file)
         self.texts = self.df['text'].tolist()
         self.labels = self.df['label'].tolist()
+        self.shuffle = shuffle
 
     def __len__(self):
         return len(self.df)
 
     def __getitem__(self, idx):
         return (self.texts[idx], self.labels[idx])
+
+    @property
+    def shuffled(self):
+        return self.shuffle
 
 # Step 1: Load your fine-tuned model and tokenizer
 model = DistilBertForSequenceClassification.from_pretrained("/rscratch/tpang/kinshuk/RpMKin/distilbert/model")
@@ -25,7 +30,7 @@ tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 # Step 2: Wrap your model with TextAttack's wrapper 
 model_wrapper = HuggingFaceModelWrapper(model, tokenizer)
 
-# Step 3: Choose an attack (3-4 attacks to be run)
+# Step 3: Choose an attack
 attack = TextFoolerJin2019.build(model_wrapper)
 
 # Step 4: Load your dataset
@@ -36,8 +41,3 @@ attack_args = AttackArgs(num_examples=int(0.01*len(dataset)))
 attacker = Attacker(attack, dataset, attack_args)
 res = attacker.attack_dataset()
 print(res.__dict__)
-# hotflip attack
-# bert attack
-# textfooler
-# input reduction
-# deepwordbug
