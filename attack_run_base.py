b9ccb9f751f12e6fbb090f6f92e36de15fd5be35
Kinshuk Goel <103813028+RpM-Kinshuk@users.noreply.github.com>
2023-11-16 23:21:26 +0530

    Added comments

--- attack_run_base.py
+++ attack_run_base.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import textattack
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from transformers import DistilBertForSequenceClassification, AutoTokenizer
 from textattack.models.wrappers import HuggingFaceModelWrapper
 from textattack.datasets import Dataset
 from textattack.attack_recipes import TextFoolerJin2019
-from textattack.data_augmentation import Augmenter
 from textattack import AttackArgs, Attacker
 
 class CustomDataset(Dataset):
@@ -20,8 +19,8 @@ class CustomDataset(Dataset):
         return (self.texts[idx], self.labels[idx])
 
 # Step 1: Load your fine-tuned model and tokenizer
-model = AutoModelForSequenceClassification.from_pretrained("path_to_your_model")
-tokenizer = AutoTokenizer.from_pretrained("path_to_your_model")
+model = DistilBertForSequenceClassification.from_pretrained("/rscratch/tpang/kinshuk/RpMKin/distilbert/model")
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 
 # Step 2: Wrap your model with TextAttack's wrapper 
 model_wrapper = HuggingFaceModelWrapper(model, tokenizer)
@@ -30,9 +29,15 @@ model_wrapper = HuggingFaceModelWrapper(model, tokenizer)
 attack = TextFoolerJin2019.build(model_wrapper)
 
 # Step 4: Load your dataset
-dataset = CustomDataset('dataset/data.csv')
+dataset = CustomDataset('/rscratch/tpang/kinshuk/RpMKin/distilbert/dataset/english/data.csv')
 
 # Step 5: Run the attack
-attack_args = AttackArgs(num_examples=len(dataset))
+attack_args = AttackArgs(num_examples=int(0.01*len(dataset)))
 attacker = Attacker(attack, dataset, attack_args)
-attacker.attack_dataset()
+res = attacker.attack_dataset()
+print(res.__dict__)
+# hotflip attack
+# bert attack
+# textfooler
+# input reduction
+# deepwordbug
